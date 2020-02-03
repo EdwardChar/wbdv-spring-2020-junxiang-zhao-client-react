3268eb54335d6bad448a7445fe9e751448c8198d
edwardchar <edwardchar@yahoo.com>
2020-02-02 23:06:46 -0500

    add CourseServices

---
+++ src/services/CourseServices.js
@@ -0,0 +1,36 @@
+const API_URL = "https://wbdv-generic-server.herokuapp.com/api/mcuks/courses";
+
+export const updateCourse = async (courseId, course) => {
+    const response = await fetch(`${API_URL}/${courseId}`, {
+        method: 'PUT',
+        body: JSON.stringify(course),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+    return await response.json()
+}
+
+export const deleteCourse = async (courseId) => {
+    const response = await fetch(`${API_URL}/${courseId}`, {
+        method: 'DELETE'
+    })
+    return await response.json()
+}
+
+export const createCourse = async (course) =>
+{
+    const response = await fetch(API_URL, {
+        method: "POST",
+        body: JSON.stringify(course),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+    return await response.json()
+}
+
+export const findAllCourses = () => {
+    return fetch(API_URL)
+        .then(response => response.json())
+}
