9d7585f67bd66a3a57e4311be063a3864c0db56c
edwardchar <edwardchar@yahoo.com>
2020-02-04 16:35:42 -0500

    add topic pills

---
+++ src/components/CourseEditor/TopicPills.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TopicPills = () =>
+    <div className="btn-group wbdv-topic-pill-list" role="group" aria-label="Basic example">
+        <button type="button" className="btn btn-secondary wbdv-topic-pill">Topic 1</button>
+        <button type="button" className="btn btn-secondary wbdv-topic-pill">Topic 2</button>
+        <button type="button" className="btn btn-secondary wbdv-topic-pill">Topic 3</button>
+        <button type="button" className="btn btn-secondary wbdv-topic-pill">Topic 4</button>
+        <button type="button" className="btn btn-secondary wbdv-topic-add-btn"><i className="fas fa-plus"></i>
+        </button>
+    </div>
+
+export default TopicPills;
