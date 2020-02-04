6432c724c3c963b37e4d8427373615ee0b5236d4
edwardchar <edwardchar@yahoo.com>
2020-02-04 16:35:06 -0500

    add module list

---
+++ src/components/CourseEditor/ModuleList.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const ModuleList = () =>
+    <div className="col-3 pl-4 pr-3 py-3 sideArea wbdv-module-list">
+        <div className="card text-white bg-secondary my-2 wbdv-module-item">
+            <div className="card-body">
+                <span className="wbdv-module-item-title">Module 1 - jQuery</span>
+                <button type="button" className="close wbdv-module-item-delete-btn" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+        <div className="card text-white bg-secondary my-2 wbdv-module-item">
+            <div className="card-body">
+                <span className="wbdv-module-item-title">Module 2 - React</span>
+                <button type="button" className="close wbdv-module-item-delete-btn" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+        <div className="card text-white bg-secondary my-2 wbdv-module-item">
+            <div className="card-body">
+                <span className="wbdv-module-item-title">Module 3 - Redux</span>
+                <button type="button" className="close wbdv-module-item-delete-btn" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </div>
+        <button type="button" className="btn btn-danger float-right wbdv-module-item-add-btn"><i
+            className="fas fa-plus"></i></button>
+    </div>
+
+export default ModuleList;
