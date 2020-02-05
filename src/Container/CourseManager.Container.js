b00784b93915cda5ad86b40433e4baac988de400
edwardchar <edwardchar@yahoo.com>
2020-02-04 21:34:45 -0500

    add course table

--- src/Container/CourseManager.Container.js
+++ src/Container/CourseManager.Container.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./CourseManager.css"
 import {deleteCourse, createCourse, findAllCourses} from "../services/CourseServices.js";
+import CourseTableContainer from "./CourseTable.Container";
 
 class CourseManagerContainer extends React.Component {
     state = {
@@ -42,6 +43,34 @@ class CourseManagerContainer extends React.Component {
                             </button>
                     </form>
                 </nav>
+                <nav className="navbar navbar-expand-lg d-none d-lg-flex navbar-light bg-light">
+                    <div className="collapse navbar-collapse container">
+                        <div className="navbar-nav mx-auto w-100 row font-weight-bold">
+                            <div className="nav-item col-5">
+                                <a className="nav-link text-secondary wbdv-header wbdv-title" href="#">Title </a>
+                            </div>
+                            <div className="nav-item col-2 dropdown">
+                                <a className="nav-link dropdown-toggle text-secondary wbdv-header wbdv-owner" href="#">Owned
+                                    by </a>
+                            </div>
+                            <div className="nav-item col-2">
+                                <a className="nav-link text-secondary wbdv-header wbdv-last-modified" href="#">Last
+                                    modified </a>
+                            </div>
+                            <div className="nav-item col-1">
+                                <a className="nav-link text-secondary wbdv-button wbdv-grid-layout" href="#"><i
+                                    className="fas fa-grip-horizontal"></i></a>
+                            </div>
+                            <div className="nav-item col">
+                                <a className="nav-link text-secondary wbdv-header wbdv-sort" href="#"><i
+                                    className="fas fa-sort-alpha-down"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+                <div className = "container" >
+                <CourseTableContainer/>
+                </div>
             </div>
         );
     }
