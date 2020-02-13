25beab3aca00f96d285d445a9251928affc895cb
edwardchar <edwardchar@yahoo.com>
2020-02-13 08:32:21 -0500

    add topic actions

---
+++ src/actions/TopicActions.js
@@ -0,0 +1,25 @@
+export const CREATE_TOPIC = "CREATE_TOPIC";
+export const DELETE_TOPIC = "DELETE_TOPIC";
+export const FIND_TOPICS_FOR_LESSON = "FIND_TOPICS_FOR_LESSON";
+export const UPDATE_TOPIC = "UPDATE_TOPIC";
+
+export const deleteTopic = (topicId) => ({
+    type: "DELETE_TOPIC",
+    topicId: topicId
+})
+
+export const createTopic = (topic) => ({
+    type: "CREATE_TOPIC",
+    newTopic: topic
+})
+
+export const findTopicsForLesson = (topic) => ({
+    type: 'FIND_TOPICS_FOR_LESSON',
+    topics: topic
+})
+
+export const updateTopic = (topic) => ({
+    type: "UPDATE_TOPIC",
+    topicId: topic._id,
+    topic: topic
+})
