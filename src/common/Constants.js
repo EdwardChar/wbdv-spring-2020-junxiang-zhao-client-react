55fe0fb63fd4147eef5ae7e7317f16a5bcb4edda
edwardchar <edwardchar@yahoo.com>
2020-02-23 00:47:17 -0500

    add widget service

--- src/common/Constants.js
+++ src/common/Constants.js
@@ -1,7 +1,9 @@
-export const COURSES_API_URL = "https://wbdv-generic-server.herokuapp.com/api/001057049/courses"
-export const MODULES_API_URL = "https://wbdv-generic-server.herokuapp.com/api/001057049/modules"
-export const LESSONS_API_URL = "https://wbdv-generic-server.herokuapp.com/api/001057049/lessons"
-export const TOPICS_API_URL = "https://wbdv-generic-server.herokuapp.com/api/001057049/topics"
-export const COURSES_MODULES_API_URL = (courseId) => `https://wbdv-generic-server.herokuapp.com/api/001057049/courses/${courseId}/modules`
-export const MODULES_LESSONS_API_URL = (moduleId) => `https://wbdv-generic-server.herokuapp.com/api/001057049/modules/${moduleId}/lessons`
-export const LESSONS_TOPICS_API_URL = (lessonId) => `https://wbdv-generic-server.herokuapp.com/api/001057049/lessons/${lessonId}/topics`
+export const COURSES_API_URL = "https://wbdv-generic-server.herokuapp.com/api/mcuks/courses"
+export const MODULES_API_URL = "https://wbdv-generic-server.herokuapp.com/api/mcuks/modules"
+export const LESSONS_API_URL = "https://wbdv-generic-server.herokuapp.com/api/mcuks/lessons"
+export const TOPICS_API_URL = "https://wbdv-generic-server.herokuapp.com/api/mcuks/topics"
+export const WIDGETS_API_URL = "https://wbdv-generic-server.herokuapp.com/api/mcuks/widgets"
+export const COURSES_MODULES_API_URL = (courseId) => `https://wbdv-generic-server.herokuapp.com/api/mcuks/courses/${courseId}/modules`
+export const MODULES_LESSONS_API_URL = (moduleId) => `https://wbdv-generic-server.herokuapp.com/api/mcuks/modules/${moduleId}/lessons`
+export const LESSONS_TOPICS_API_URL = (lessonId) => `https://wbdv-generic-server.herokuapp.com/api/mcuks/lessons/${lessonId}/topics`
+export const WIDGETS_LESSONS_API_URL = (widgetId) => `https://wbdv-generic-server.herokuapp.com/api/mcuks/widgets/${widgetId}/widgets`
---
+++ src/services/WidgetService.js
@@ -0,0 +1,42 @@
+import {
+    WIDGETS_API_URL,
+    TOPICS_WIDGETS_API_URL,
+} from "../common/Constants";
+
+const findWidgetsForTopic = (topicId) =>
+    fetch(TOPICS_WIDGETS_API_URL(topicId))
+        .then(response => response.json());
+
+const createWidget = (topicId) =>
+    fetch(TOPICS_WIDGETS_API_URL(topicId), {
+        method: "POST",
+        body: JSON.stringify({title: "New Widget"}),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+
+const updateWidget = async (widgetId, widget) =>
+{
+    const response = await fetch(`${WIDGETS_API_URL}/${widgetId}`, {
+        method: 'PUT',
+        body: JSON.stringify(widget),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+    return await response.json()
+}
+
+const deleteWidget = (widgetId) =>
+    fetch(`${WIDGETS_API_URL}/${widgetId}`, {
+        method: "DELETE"
+    })
+        .then(response => response.json());
+
+export default {
+    createWidget,
+    deleteWidget,
+    updateWidget,
+    findWidgetsForTopic
+}
