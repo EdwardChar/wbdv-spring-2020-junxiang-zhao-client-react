52fccf685e0e8013a4e5b8dde46cf3259458d2ff
edwardchar <edwardchar@yahoo.com>
2020-03-05 17:27:46 -0500

    add widget reducer

---
+++ src/reducers/WidgetReducer.js
@@ -0,0 +1,36 @@
+import {CREATE_WIDGET, DELETE_WIDGET, FIND_WIDGETS_FOR_TOPIC, UPDATE_WIDGET} from "../actions/WidgetActions";
+
+const initialState = {
+    widgets: [
+    ]
+}
+
+const widgetReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case FIND_WIDGETS_FOR_TOPIC:
+            return {
+                widgets: action.widgets
+            }
+        case CREATE_WIDGET:
+            return {
+                widgets: [
+                    ...state.widgets,
+                    action.newWidget
+                ]
+            }
+        case DELETE_WIDGET:
+            return {
+                widgets: state.widgets.filter(widget => widget._id !== action.widgetId)
+            }
+        case UPDATE_WIDGET:
+            return {
+                widgets: state.widgets.map(widget =>
+                    widget._id === action.widgetId ? action.widget : widget
+                )
+            }
+        default:
+            return state
+    }
+}
+
+export default widgetReducer
