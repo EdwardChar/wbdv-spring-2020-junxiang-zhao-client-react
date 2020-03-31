9666f9bb067da5090c472c54f4acc9850bec58f5
edwardchar <edwardchar@yahoo.com>
2020-03-31 00:13:20 -0400

    add WidgetActions

---
+++ src/actions/WidgetActions.js
@@ -0,0 +1,25 @@
+export const CREATE_WIDGET = "CREATE_WIDGET";
+export const DELETE_WIDGET = "DELETE_WIDGET";
+export const FIND_WIDGETS_FOR_TOPIC = "FIND_WIDGETS_FOR_TOPIC";
+export const UPDATE_WIDGET = "UPDATE_WIDGET";
+
+export const deleteWidget = (widgetId) => ({
+    type: "DELETE_WIDGET",
+    widgetId: widgetId
+})
+
+export const createWidget = (widget) => ({
+    type: "CREATE_WIDGET",
+    newWidget: widget
+})
+
+export const findWidgetsForTopic = (widget) => ({
+    type: 'FIND_WIDGETS_FOR_TOPIC',
+    widgets: widget
+})
+
+export const updateWidget = (widget) => ({
+    type: "UPDATE_WIDGET",
+    widgetId: widget.id,
+    widget: widget
+})
